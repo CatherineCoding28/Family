8bcfd8e07b375fb1f81a42bad18846c055963a8e
CatherineCoding28 <catherinecoding28@gmail.com>
2025-05-06 20:56:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var num = 0;
+var Photo = ["familyPhoto.webp", "dad.jpg", "mom.jpg", "girl1.jpg", "girl2.jpg"];
+var relationship = ["", "Dad", "Mom", "Me", "Sister"];
+var names = ["", "Mikhail", "Vera", "Catherine", "Nicole"];
+
+function right(){
+    if (num<4) {
+        num = num + 1;
+    }
+    else {
+        num = 0;
+    }
+    document.getElementById("pic").src=Photo[num];
+    document.getElementById("relationship").innerHTML=relationship[num];
+    document.getElementById("name").innerHTML=names[num];
+}
