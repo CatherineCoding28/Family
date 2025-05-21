652602bd0b4a7652e106227556b2991524f05579
CatherineCoding28 <catherinecoding28@gmail.com>
2025-05-20 20:15:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ var relationship = ["", "Dad", "Mom", "Me", "Sister"];
 var names = ["", "Mikhail", "Vera", "Catherine", "Nicole"];
 
 function right(){
-    if (num<4) {
+    if (num<4){
         num = num + 1;
     }
-    else {
+    else{
         num = 0;
     }
-    document.getElementById("pic").src=Photo[num];
     document.getElementById("relationship").innerHTML=relationship[num];
     document.getElementById("name").innerHTML=names[num];
+    document.getElementById("pic").src=Photo[num];
+}
+
+function left(){
+    if (num>0){
+        num = num - 1;
+    }
+    else{
+        num = 4;
+    }
+    document.getElementById("relationship").innerHTML=relationship[num];
+    document.getElementById("name").innerHTML=names[num];
+    document.getElementById("pic").src=Photo[num];
 }
